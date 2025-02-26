1177f699aec51a4ed6c5131a23d86e280a82bdf1
Filipe Mallmann Siota <filipesiota@gmail.com>
2025-02-25 22:18:17 -0300

    FEAT: Implement deleting a student from the system

--- registration_system.c
+++ registration_system.c
@@ -7,6 +7,7 @@
 #define NAME_LENGTH 50
 #define SUBJECTS_LENGTH 8
 
+// Struct definitions
 typedef struct {
 	int day;
 	int month;
@@ -27,13 +28,15 @@ typedef struct {
 	int size;
 } StudentArray;
 
-void clearString(char *string);
+// Helper functions
+void normalizeString(char *string);
 
+// Action functions
 void register_student(StudentArray *studentArray);
-void delete_student(StudentArray *studentArray, char *registration);
-void update_student_subjects(StudentArray *studentArray, char *registration);
+void delete_student(StudentArray *studentArray);
+void update_student_subjects(StudentArray *studentArray);
 void show_students(StudentArray *studentArray);
-void show_student(StudentArray *studentArray, char *registration);
+void show_student(StudentArray *studentArray);
 
 int main() {
 	StudentArray studentArray;
@@ -46,15 +49,27 @@ int main() {
 	
 	register_student(&studentArray);
 	
-	for (i = 0; i < studentArray.size; i++) {
-		printf("\nNome do aluno cadastrado: %s", studentArray.data[i].name);
+	int curSize = studentArray.size;
+	
+	delete_student(&studentArray);
+	
+	int newSize = studentArray.size;
+	
+	system("cls");
+	printf("UPDATED ARRAY SIZE: %d", curSize);
+	printf("\nUPDATED ARRAY SIZE: %d", newSize);
+	
+	if (studentArray.size > 0) {
+		free(studentArray.data);
 	}
 	
-	free(studentArray.data);
+	printf("\n\n# Programa finalizado!\n");
+	system("pause");
+	
 	return 0;
 }
 
-void clearString(char *string) {
+void normalizeString(char *string) {
 	size_t len = strlen(string);
 	
 	if (len > 0 && string[len - 1] == '\n') {
@@ -74,17 +89,17 @@ void register_student(StudentArray *studentArray) {
 	printf("Número de matrícula: ");
 	fflush(stdin);
 	fgets(student.registration, CODE_LENGTH, stdin);
-	clearString(student.registration);
+	normalizeString(student.registration);
 	
 	printf("Nome: ");
 	fflush(stdin);
 	fgets(student.name, NAME_LENGTH, stdin);
-	clearString(student.name);
+	normalizeString(student.name);
 	
 	printf("Código do curso: ");
 	fflush(stdin);
 	fgets(student.course_code, CODE_LENGTH, stdin);
-	clearString(student.course_code);
+	normalizeString(student.course_code);
 	
 	do {
 		printf("Data de nascimento (DD/MM/AAAA): ");
@@ -109,7 +124,7 @@ void register_student(StudentArray *studentArray) {
 			printf("Informe o código de uma disciplina que o aluno esteja cursando ou 0 para finalizar o cadastro: ");
 			fflush(stdin);
 			fgets(student.subject_codes[i], CODE_LENGTH, stdin);
-			clearString(student.subject_codes[i]);
+			normalizeString(student.subject_codes[i]);
 			
 			if (strcmp(student.subject_codes[i], "0") == 0) {
 				askForSubject = 0;
@@ -134,11 +149,50 @@ void register_student(StudentArray *studentArray) {
 	system("pause");
 }
 
-void delete_student(StudentArray *studentArray, char *registration) {
+void delete_student(StudentArray *studentArray) {
+	int newSize = studentArray->size - 1;
+	Student *temp = (Student *)malloc(newSize * sizeof(Student));
+	char registration[CODE_LENGTH];
+	int tempCounter;
+	int i;
 	
+	system("cls");
+	printf("# Excluir aluno\n\n");
+	
+	do {
+		printf("Informe o número da matrícula do aluno que deseja excluir: ");
+		fflush(stdin);
+		fgets(registration, CODE_LENGTH, stdin);
+		normalizeString(registration);
+		
+		tempCounter = 0;
+		
+		for (i = 0; i < studentArray->size; i++) {
+			if (strcmp(studentArray->data[i].registration, registration) == 0) {
+				continue;
+			}
+			
+			if (tempCounter < newSize) {
+				temp[tempCounter] = studentArray->data[i];
+			}
+			
+			tempCounter++;
+		}
+		
+		if (tempCounter > newSize) {
+			printf("\nMatrícula não encontrada!\n\n");
+		}
+	} while (tempCounter > newSize);
+	
+	studentArray->data = temp;
+	studentArray->size--;
+	free(temp);
+	
+	printf("\n# Exclusão finalizada!\n");
+	system("pause");
 }
 
-void update_student_subjects(StudentArray *studentArray, char *registration) {
+void update_student_subjects(StudentArray *studentArray) {
 	
 }
 
@@ -146,7 +200,7 @@ void show_students(StudentArray *studentArray) {
 	
 }
 
-void show_student(StudentArray *studentArray, char *registration) {
+void show_student(StudentArray *studentArray) {
 	
 }
 
