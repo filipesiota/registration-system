d1db79ee27191a0d75c23ebd144dc6131e940af4
Filipe Mallmann Siota <filipesiota@gmail.com>
2025-04-01 21:55:29 -0300

    feat: add search sample for algorithm complexity folder

---
+++ samples/algorithm-complexity/search/main.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAIN_ARRAY_LENGTH 30
+
+int linear_search(int *array, int length, int target);
+int linear_search_with_sentinel(int *array, int length, int target);
+int linear_search_sorted(int *array, int length, int target);
+int binary_search(int *array, int length, int target);
+int interpolation_search(int *array, int length, int target);
+int approximate_search(int *array, int length, int target);
+
+int main() {
+	int array[MAIN_ARRAY_LENGTH + 1] = {
+	     2,  5,  8, 11, 14,
+	    17, 20, 23, 26, 29,
+	    32, 35, 38, 41, 44,
+	    47, 50, 53, 56, 59,
+	    62, 65, 68, 71, 74,
+	    77, 80, 83, 86, 89
+	};
+	
+	int target = 89;
+	
+	printf("Linear Search: %d\n", linear_search(&array, MAIN_ARRAY_LENGTH, target));
+	printf("Linear Search With Sentinel: %d\n", linear_search_with_sentinel(&array, MAIN_ARRAY_LENGTH, target));
+	printf("Linear Search in Sorted List: %d\n", linear_search_sorted(&array, MAIN_ARRAY_LENGTH, target));
+	printf("Binary Search: %d\n", binary_search(&array, MAIN_ARRAY_LENGTH, target));
+	printf("Interpolation Search: %d\n", interpolation_search(&array, MAIN_ARRAY_LENGTH, target));
+	printf("Approximate Search: %d\n", approximate_search(&array, MAIN_ARRAY_LENGTH, target));
+	
+	return 0;
+}
+
+int linear_search(int *array, int length, int target) {
+	int i;
+	
+	for (i = 0; i < length; i++) {
+		if (array[i] == target) {
+			return i;
+		}
+	}
+	
+	return -1;
+}
+
+int linear_search_with_sentinel(int *array, int length, int target) {
+	int i = 0;
+	array[length] = target;
+	
+	while (array[i] != target) {
+		i++;
+	}
+	
+	if (i < length) {
+		return i;
+	}
+	
+	return -1;
+}
+
+int linear_search_sorted(int *array, int length, int target) {
+	int i = 0;
+	array[length] = target + 1;
+	
+	while (array[i] < target) {
+		i++;
+	}
+	
+	if (array[i] == target) {
+		return i;
+	}
+	
+	return -1;
+}
+
+int binary_search(int *array, int length, int target) {
+	int head = 0;
+	int tail = length - 1;
+	
+	while (head <= tail) {
+		int middle = (head + tail) / 2;
+		
+		if (array[middle] == target) {
+			return middle;
+		}
+		
+		if (array[middle] > target) {
+			tail = middle - 1;
+		} else {
+			head = middle + 1;
+		}
+	}
+	
+	return -1;
+}
+
+int interpolation_search(int *array, int length, int target) {	
+	return -1;
+}
+
+int approximate_search(int *array, int length, int target) {
+	return -1;
+}
+
