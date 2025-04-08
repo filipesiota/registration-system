acc571427cc580299a1f516cd9d5040ed2a8813a
Filipe Mallmann Siota <filipe.siota@diarioescola.com.br>
2025-04-07 23:47:02 -0300

    refact: use dynamic array strategy inside search sample

--- samples/algorithm-complexity/search/main.c
+++ samples/algorithm-complexity/search/main.c
@@ -11,23 +11,32 @@ int interpolation_search(int *array, int length, int target);
 int approximate_search(int *array, int length, int target);
 
 int main() {
-	int array[MAIN_ARRAY_LENGTH + 1] = {
-	     2,  5,  8, 11, 14,
-	    17, 20, 23, 26, 29,
-	    32, 35, 38, 41, 44,
-	    47, 50, 53, 56, 59,
-	    62, 65, 68, 71, 74,
-	    77, 80, 83, 86, 89
-	};
+	int search_array[MAIN_ARRAY_LENGTH] = {
+		2,  5,  8, 11, 14,
+	   17, 20, 23, 26, 29,
+	   32, 35, 38, 41, 44,
+	   47, 50, 53, 56, 59,
+	   62, 65, 68, 71, 74,
+	   77, 80, 83, 86, 89
+   };
+
+	int *array = malloc(MAIN_ARRAY_LENGTH * sizeof(int));
+	int target;
+	int i;
+
+	for (i = 0; i < MAIN_ARRAY_LENGTH; i++) {
+		array[i] = search_array[i];
+	}
+
+	printf("Informe um numero para procurar no vetor: ");
+	scanf("%d", &target);
 	
-	int target = 89;
-	
-	printf("Linear Search: %d\n", linear_search(&array, MAIN_ARRAY_LENGTH, target));
-	printf("Linear Search With Sentinel: %d\n", linear_search_with_sentinel(&array, MAIN_ARRAY_LENGTH, target));
-	printf("Linear Search in Sorted List: %d\n", linear_search_sorted(&array, MAIN_ARRAY_LENGTH, target));
-	printf("Binary Search: %d\n", binary_search(&array, MAIN_ARRAY_LENGTH, target));
-	printf("Interpolation Search: %d\n", interpolation_search(&array, MAIN_ARRAY_LENGTH, target));
-	printf("Approximate Search: %d\n", approximate_search(&array, MAIN_ARRAY_LENGTH, target));
+	printf("Linear Search: %d\n", linear_search(array, MAIN_ARRAY_LENGTH, target));
+	printf("Linear Search With Sentinel: %d\n", linear_search_with_sentinel(array, MAIN_ARRAY_LENGTH, target));
+	printf("Linear Search in Sorted List: %d\n", linear_search_sorted(array, MAIN_ARRAY_LENGTH, target));
+	printf("Binary Search: %d\n", binary_search(array, MAIN_ARRAY_LENGTH, target));
+	printf("Interpolation Search: %d\n", interpolation_search(array, MAIN_ARRAY_LENGTH, target));
+	printf("Approximate Search: %d\n", approximate_search(array, MAIN_ARRAY_LENGTH, target));
 	
 	return 0;
 }
@@ -46,6 +55,8 @@ int linear_search(int *array, int length, int target) {
 
 int linear_search_with_sentinel(int *array, int length, int target) {
 	int i = 0;
+
+	array = realloc(array, (length + 1) * sizeof(int));
 	array[length] = target;
 	
 	while (array[i] != target) {
@@ -61,6 +72,8 @@ int linear_search_with_sentinel(int *array, int length, int target) {
 
 int linear_search_sorted(int *array, int length, int target) {
 	int i = 0;
+
+	array = realloc(array, (length + 1) * sizeof(int));
 	array[length] = target + 1;
 	
 	while (array[i] < target) {
@@ -95,7 +108,20 @@ int binary_search(int *array, int length, int target) {
 	return -1;
 }
 
-int interpolation_search(int *array, int length, int target) {	
+int interpolation_search(int *array, int length, int target) {
+	// int head = 0;
+	// int tail = length - 1;
+
+	// while (head <= tail && target >= array[head] && target <= array[tail]) {
+	// 	int middle;
+
+	// 	if (array[head] == array[tail]) {
+	// 		middle = head;
+	// 	} else {
+	// 		middle = 
+	// 	}
+	// }
+
 	return -1;
 }
 
