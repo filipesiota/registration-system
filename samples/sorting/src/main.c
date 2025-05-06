eb58bac9174c1aaab778b9715d4b16302116d5b3
Filipe Mallmann Siota <filipesiota@gmail.com>
2025-05-06 00:45:36 -0300

    feat: allow creating bigger examples

--- samples/sorting/src/main.c
+++ samples/sorting/src/main.c
@@ -1,25 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "integer_array_list.h"
 #include "insertion_sort.h"
 #include "bogosort.h"
 #include "sorting_utils.h"
 
+#define TEST_RANGE 100
+#define TEST_RAND_RANGE 100
+
 int main() {
     int range;
     IntegerArrayList *list = initIntegerArrayList();
 
-    printf("Informe o tamanho do vetor: ");
-    scanf("%d", &range);
+    int test[TEST_RANGE];
+    srand(time(NULL));
 
-    int i;
-    for (i = 0; i < range; i++) {
-        int n;
-        printf("Valor %d: ", i + 1);
-        scanf("%d", &n);
-
-        addIntegerArrayList(list, n);
+    for (int i = 0; i < TEST_RANGE; i++) {
+        test[i] = rand() % TEST_RAND_RANGE;
     }
+
+    for (int i = 0; i < TEST_RANGE; i++) {
+        addIntegerArrayList(list, test[i]);
+    }
+
+    // printf("Informe o tamanho do vetor: ");
+    // scanf("%d", &range);
+
+    // int i;
+    // for (i = 0; i < range; i++) {
+    //     int n;
+    //     printf("Valor %d: ", i + 1);
+    //     scanf("%d", &n);
+
+    //     addIntegerArrayList(list, n);
+    // }
 
     IntegerArrayList *listCopyForInsertionSort = copyIntegerArrayList(list);
     IntegerArrayList *listCopyForBogosort = copyIntegerArrayList(list);
@@ -27,19 +42,18 @@ int main() {
     IntegerArrayListSorted *insertionSortResult = insertionSort(listCopyForInsertionSort);
     IntegerArrayListSorted *bogosortResult = bogosort(listCopyForBogosort);
 
-    printf("\n# Insertion Sort:");
-    printf("\nResultado: ");
+    printf("\n[Insertion Sort] Resultado: ");
     printIntegerArrayList(insertionSortResult->sortedList);
-    printf("Tempo de execucao: %.10lf ms\n", insertionSortResult->executionTime);
-
-    printf("\n# Bogosort:");
-    printf("\nResultado: ");
+    printf("[Bogosort] Resultado: ");
     printIntegerArrayList(bogosortResult->sortedList);
 
+    printf("\nOverview:\n\n");
+    printf("[Insertion Sort] Tempo de execucao: %.10lf ms\n", insertionSortResult->executionTime);
+
     if (bogosortResult->executionTime > -1.0) {
-        printf("Tempo de execucao: %.10lf ms\n\n", bogosortResult->executionTime);
+        printf("[Bogosort] Tempo de execucao: %.10lf ms\n\n", bogosortResult->executionTime);
     } else {
-        printf("Tempo de execucao: Nao foi concluido (demorou mais que %d ms)\n\n", BOGOSORT_MAX_EXECUTION_MS);
+        printf("[Bogosort] Tempo de execucao: Nao foi concluido (demorou mais que %d ms)\n\n", BOGOSORT_MAX_EXECUTION_MS);
     }
 
     sysCommand("pause_terminal");
